4a4fa0cb81a5a28c1298104ecfcba36c9e06ccc4
EricLiz <forsagxd@gmail.com>
2022-03-06 21:45:07 -0300

    edit method added

--- src/main/java/com/ericliz/cryptoportifolio/controller/ItemController.java
+++ src/main/java/com/ericliz/cryptoportifolio/controller/ItemController.java
@@ -1,24 +1,58 @@
 package com.ericliz.cryptoportifolio.controller;
 
+import com.ericliz.cryptoportifolio.dto.ItemDTO;
 import com.ericliz.cryptoportifolio.model.Item;
 import com.ericliz.cryptoportifolio.repository.ItemRepository;
+import com.ericliz.cryptoportifolio.service.ItemService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/items")
 @AllArgsConstructor
 public class ItemController {
 
-    private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping
-    public List<Item> hello(){
-        return itemRepository.findAll();
+    public ResponseEntity<List<Item>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(itemService.findAll());
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable (value ="id") long id){
+        Optional<Item> itemModelOptional = itemService.findById(id);
+        if(!itemModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(itemModelOptional.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateItem(@PathVariable(value="id")long id,
+                                             @RequestBody @Valid ItemDTO itemDTO){
+        Optional<Item> itemModelOptional = itemService.findById(id);
+        if(!itemModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found.");
+        }
+        Item itemModel = new Item();
+        BeanUtils.copyProperties(itemDTO, itemModel);
+        itemModel.setId(itemModelOptional.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(itemService.save(itemModel));
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveItem(@RequestBody @Valid ItemDTO itemDTO){
+        Item itemModel = new Item();
+        BeanUtils.copyProperties(itemDTO, itemModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(itemService.save(itemModel));
     }
 }
--- src/main/java/com/ericliz/cryptoportifolio/model/Item.java
+++ src/main/java/com/ericliz/cryptoportifolio/model/Item.java
@@ -1,19 +1,22 @@
 package com.ericliz.cryptoportifolio.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Data
 @Entity
 public class Item {
     @Id
-    @JsonProperty("_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     @Column(length = 15,nullable = false)
     private String name;
+
+    @Column(nullable = false, precision=10, scale=2)
+    private BigDecimal price;
+
     @Column(length = 15,nullable = false)
     private String category;
 
