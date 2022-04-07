c2268c172012ca4e90116ee043e33716b380923e
Eric-Liz <forsagxd@gmail.com>
2022-04-07 17:26:31 -0300

    get currency price in binance api

--- src/main/java/com/ericliz/cryptoportifolio/controller/BinanceController.java
+++ src/main/java/com/ericliz/cryptoportifolio/controller/BinanceController.java
@@ -7,14 +7,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/currencies")
+@RequestMapping("/api/currencies/")
 @AllArgsConstructor
 public class BinanceController {
     private final BinanceService binanceService;
-    @GetMapping("/actualPrice")
+    List<BinanceAvgPrice> avgPriceList;
+
+    @GetMapping("/avg/actualPrice")
     public ResponseEntity<BinanceAvgPrice> getAvgPrice(@RequestParam String symbol){
         return ResponseEntity.status(HttpStatus.OK).body(binanceService.getAvgPrice(symbol));
+    }
+    @PostMapping("/avg")
+    public ResponseEntity<List<BinanceAvgPrice>> getGroupAvgPrice(@RequestBody @Valid List<String> currencySymbols){
+        return ResponseEntity.status(HttpStatus.OK).body(binanceService.getAvgPriceList(currencySymbols));
     }
 
 }
--- src/main/java/com/ericliz/cryptoportifolio/service/BinanceService.java
+++ src/main/java/com/ericliz/cryptoportifolio/service/BinanceService.java
@@ -3,11 +3,23 @@ package com.ericliz.cryptoportifolio.service;
 import com.ericliz.cryptoportifolio.model.BinanceAvgPrice;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BinanceService {
+
     public BinanceAvgPrice getAvgPrice(String symbol){
         RestTemplate restTemplate = new RestTemplate();
         BinanceAvgPrice avgPrice = restTemplate.getForObject("https://api.binance.com/api/v3/avgPrice?symbol="+symbol.toUpperCase()+"USDT", BinanceAvgPrice.class);
         return avgPrice;
+    }
+    public List<BinanceAvgPrice> getAvgPriceList(List<String> symbolList){
+        List<BinanceAvgPrice> avgPriceList = new ArrayList<>();
+        for (String symbol : symbolList ) {
+            avgPriceList.add(getAvgPrice(symbol));
+        }
+        return avgPriceList;
     }
 }
